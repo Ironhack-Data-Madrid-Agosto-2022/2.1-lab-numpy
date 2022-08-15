707f87727687f47a51a3d17fff9776fd1b4818d0
Alonso Herrera <aherreraitie@gmail.com>
2022-08-15 17:57:40 +0200

    solutioons numpy

--- main.py
+++ main.py
@@ -1,68 +1,74 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+np.version.version
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a= np.random.random((2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.full((5,2,3), 1)
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+print(a.size)
+print(b.size)
+#yes, the size for both of them is 30
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+np.add(a,b)
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c= b.transpose(1,2,0)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = np.add(a,c)
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print('this is a ', a)
 
-
+print('this is d', d)
 
 
 #12. Multiply a and c. Assign the result to e.
-
+e = np.multiply(a,c)
 
 
 #13. Does e equal to a? Why or why not?
 
-
+e == a # because all c values are equal to 1
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = d.max()
+d_min= d.min()
+d_mean = d.mean()
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty([2,3,5])
 
 
 """
@@ -74,6 +80,28 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+a=0
+b=0
+c=0
+for x in d:
+    for y in x:
+        for z in y:
+            if z>d_min and z<d_mean:
+                f[a][b][c]=25
+            if z>d_mean and z<d_max:
+                f[a][b][c]=75
+            if z==d_mean:
+                f[a][b][c]=50
+            if z==d_min:
+                f[a][b][c]=0
+            if z==d_max:
+                f[a][b][c]=100
+            c+=1
+        c=0
+        b+=1
+    b=0
+    a+=1
+print(f)
 
 
 
@@ -89,17 +117,20 @@ array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.79129243, 1.74983003, 1.96028037, 1.85166831, 1.65450881],
         [1.18068344, 1.9587381 , 1.00656599, 1.93402165, 1.73514584]]])
 
+
+
 Your f should be:
 array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  25.,  25.,  25.],
         [ 75.,  25.,  75.,  75.,  75.]],
 
        [[ 25.,  25.,  25.,  25., 100.],
-        [ 75.,  75.,  75.,  75.,  75.],
+       [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
-
+print(d)
+print(f)
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
