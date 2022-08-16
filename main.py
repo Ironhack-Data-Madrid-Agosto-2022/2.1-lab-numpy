8c7176100491cf0597e8b54c3b98239384495e83
joserojas95 <110819726+joserojas95@users.noreply.github.com>
2022-08-16 10:14:33 +0200

    2.1-lab-numpy

--- main.py
+++ main.py
@@ -1,72 +1,92 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
+print(np.__version__)
 
+np.show_config()
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2, 3,5))
+a
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
+print (b)
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(b.shape)
+print(a.shape)
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+Ambas matrices tiene el mismo número de elementos(30), pero no tienen la misma forma. 
+                                                 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c= np.reshape(b,[2, 3, 5])
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d=np.add(a, c)
+print(d)
 
+Se pueden unir ya que tiene la misma forma. 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+Print(a)
+Print(b)
 
+Si, se han sumado los valores de la matriz C a los valores de A. Tenemos la misma estructura. Es decir, se ha sumado el valor de la matriz A ( cubo 1, fila 1 y columna 1) a los valores de la matriz C ( cubo 1, fila 1 y columna 1).
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = np.multiply(a, c)
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
+Si, estamos multiplicando por la matriz C compuesta por valores 1, cualquier número por 1 es el mismo. En caso de tener valores diferentes a 1 el resultado no se´ria el mismo. 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max=d.max()
+print(d_max)
 
+d_min=d.min()
+print(d_max)
+
+d_mean=d.mean()
+print(d_max)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty((2, 3, 5))
+print (f)
 
-
-
-"""
-#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
+"""#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
 If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
@@ -75,7 +95,23 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+for i in range(2):
+    for j in range(3):
+        for x in range(5):
+           
+            if d[i][j][x] > d_min and x < d_mean:
+                f[i][j][x]=25
+            if d[i][j][x] > d_mean and x < d_max:
+                f[i][j][x]=75
+            if d[i][j][x]==d_mean:
+                f[i][j][x]=50
+            if d[i][j][x] == d.min():
+                f[i][j][x]=0
+            if d[i][j][x] == d.max():
+                f[i][j][x]=100
+                
+            
+print(f)
 
 
 """
@@ -99,6 +135,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+print(d)
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
